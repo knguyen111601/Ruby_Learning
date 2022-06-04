d2052a84cc47ade181f64c321d7de42bc00217c8
knguyen111601 <knguyen111601@gmail.com>
2022-06-04 13:18:52 -0700

    Functions

---
+++ 3.Functions/1.Functions.rb
@@ -0,0 +1,24 @@
+########################################################
+# Functions
+########################################################
+
+# Start with keyword def
+# No parenthesis for parameters
+def sayHello
+    puts "Hello"
+end
+
+# Call function, still no parenthesis since no params
+sayHello
+
+########################################################
+# Parameters
+########################################################
+# Similar to python syntax
+def addNums (a,b)
+    puts a+b
+end
+
+addNums(5,5)
+addNums(4,4)
+
