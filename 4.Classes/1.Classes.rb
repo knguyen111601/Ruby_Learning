fa34790a76824e7411bdd67eba31d6d88d47feb9
knguyen111601 <knguyen111601@gmail.com>
2022-06-04 20:26:21 -0700

    ruby classes

---
+++ 4.Classes/1.Classes.rb
@@ -0,0 +1,57 @@
+########################################################
+# Classes
+########################################################
+
+class Dog
+    # This is the constructor function
+    def initialize (name, age)
+        # Instance variables uses @ symbol 
+        # Is like this in JS
+        @name = name
+        @age = age
+    end
+    
+    def bark
+        puts "Woof!"
+    end
+
+    # All of the properties in a class is private in Ruby so we need getter and setter functions
+    # When a property is private, it cannot be accessed outside of the class
+        ## This means sparky.name wouldn't work here
+
+    def nameGetter
+        return  @name
+    end
+
+    def ageGetter
+        return @age
+    end
+
+end
+
+# Uses .new instead of new Dog
+sparky = Dog.new("Sparky", 3)
+
+sparky.bark # Woof!
+puts sparky.nameGetter # Sparky
+puts sparky.ageGetter # 3
+
+########################################################
+# Inheritance
+########################################################
+
+class Terrier < Dog
+    # OVERRRIDE
+    # Overrides the original bark function
+
+    
+
+    def bark
+        puts "Ruff!"
+    end
+end
+
+chip = Terrier.new("Chip", 2)
+chip.bark # Ruff! (override the original bark funciton)
+puts chip.nameGetter
+puts chip.ageGetter
